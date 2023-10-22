a4ec9ac5dc76e77d08e8ec6b3c2bd2be9c5e525c
Ignacio <ignacio9781@gmail.com>
2023-10-22 12:40:03 -0300

    variables y tipos de datos

---
+++ Basico/01_variables.py
@@ -0,0 +1,24 @@
+# Variables
+myvariables = 'Mi primer variables'
+print(myvariables)
+
+my_in_varible = 5
+print(my_in_varible)
+
+my_bool_variable = False
+print(my_bool_variable)
+
+
+"""
+Como nombrar las variables
+
+firtname
+lastname
+age
+first_name snakecase
+_if
+num1
+current_year_2023
+
+"""
+
