c27796b8fff87b7aaba2a7e28b2d4d08ef9ff3a4
Ignacio <ignacio9781@gmail.com>
2023-10-22 12:17:43 -0300

    primer print y tipos de datos

---
+++ Basico/00_hello_world.py
@@ -0,0 +1,17 @@
+# Hola Mundo
+
+print("Hola Python")
+
+"""
+Este es un comentario
+en varias lineas
+
+"""
+
+# Consultar el tipo de dato
+print(type('Hola Mundo')) # Tipo 'str'
+print(type(5)) # Tipo 'int'
+print(type(1.5)) # Tipo 'float'
+print(type(True)) # Tipo 'bool'
+print(type(2 + 1j)) # Tipo 'complex'
+
